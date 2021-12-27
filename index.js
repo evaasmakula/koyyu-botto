73414bdd8cc10f4c3f8259a0727e7587623ca145
evaasmakula <andikacahargen2@gmail.com>
2021-12-27 10:21:44 +0700

    adding some feature and bug fix

--- index.js
+++ index.js
@@ -1,37 +1,31 @@
 const { default: makeWASocket, useSingleFileAuthState, DisconnectReason } = require("@adiwajshing/baileys-md")
 const { state, saveState } = useSingleFileAuthState('./login.json');
-const fs = require('fs');
-const logDB = './log/Log_VideoMessage.json';
-const handler = require('./lib/handler.js');
 const errorHandler = require("./lib/errorHandler");
+const handler = require('./lib/handler.js');
+const fs = require('fs');
 
 const startSock = () => {
-    const conn = makeWASocket({ printQRInTerminal: true, auth: state })
+    const conn = makeWASocket({ printQRInTerminal: true, auth: state });
 
     conn.ev.on('messages.upsert', async m => {
-        const message = m.messages[0]
-
-        // send read receipt
+        const message = m.messages[0];
         
         if(message.key.remoteJid == "status@broadcast"){
             return;
-        }
+        };
         
         if (!message.key.fromMe && m.type === 'notify') {
-            
-            // optional logging to get last message received
-            // fs.writeFileSync(logDB, JSON.stringify(m));
-            
             try {
                 await handler(conn, message);
-                await conn.sendReadReceipt(message.key.remoteJid, message.key.participant, [message.key.id])
+                console.log("[INFO] Message sended to recipient");
+                
             } catch (err) {
                 const error = err.message;
                 console.log(error);
                 await errorHandler(conn, message, error)
-                await conn.sendReadReceipt(message.key.remoteJid, message.key.participant, [message.key.id])
-            }
-        }
+            };
+            // await conn.sendReadReceipt(message.key.remoteJid, message.key.participant, [message.key.id])
+        };
     })
 
     conn.ev.on('connection.update', (update) => {
@@ -40,7 +34,7 @@ const startSock = () => {
             lastDisconnect.error?.output?.statusCode !== DisconnectReason.loggedOut
                 ? startSock()
                 : console.log('+ connection closed')
-        }
+        };
     })
 
     conn.ev.on('creds.update', saveState)
